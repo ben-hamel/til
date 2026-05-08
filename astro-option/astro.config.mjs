dec91897273dd034015f6e8d48883da9732800d9
Ben Hamel <16502126+ben-hamel@users.noreply.github.com>
2026-05-08 09:36:54 -0230

    change base url

--- astro-option/astro.config.mjs
+++ astro-option/astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ben-hamel.github.io',
-	base: '/til-monorepo',
+	base: '/til',
 	integrations: [
 		starlight({
 			title: 'Today I Learned',
